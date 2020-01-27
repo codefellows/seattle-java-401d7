0e693bf5f1bba5f337973b1b47363e13abba91a9
Nicholas Carignan <nick.carignan@sbcglobal.net>
2020-01-27 11:36:13 -0800

    spelling

--- class-16/dragons/src/main/java/com/ferreirae/dragons/models/ApplicationUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ferreirae.dragons.models;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
-    public ApplicationUser findByUsername(String username);
-}
